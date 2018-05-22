d9a346159cfffd39eb5d4734914332fe5b7b7f59
Matpil <matpilar@users.noreply.github.com>
2018-05-21 22:25:05 -0300

    Commit

--- TpAnual/src/Modelo/Pasaporte.java
+++ TpAnual/src/Modelo/Pasaporte.java
@@ -1,5 +1,6 @@
 package Modelo;
 
 public class Pasaporte {
-
+//agregar los atrubutos gfeter setter y consytructores 
+	o//Kagregar la clase vuelos y ventas con los atrubtos fijate le tema de las relaciones que ahya algunas que lleva otros objetos
 }
