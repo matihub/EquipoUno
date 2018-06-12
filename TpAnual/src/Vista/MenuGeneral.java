fa4237e5d474df82cf71026d11bedeecb84eaf50
Matpil <matpilar@users.noreply.github.com>
2018-06-12 00:39:22 -0300

    correccion menu

---
+++ TpAnual/src/Vista/MenuGeneral.java
@@ -0,0 +1,23 @@
+package Vista;
+
+public class MenuGeneral {
+	
+	EntradaSalida es = new EntradaSalida();
+	
+	public int mostraropciones() throws Exception {
+		
+		boolean ok=false;
+		int opcion=0;
+		EntradaSalida.pritln("~~~~~~~~~~~~~~~~~~~~TP ANUAL ---VENTA DE VUELOS~~~~~~~~~~~~~~~~~~~~~~~");
+		
+		
+		
+		
+		return opcion;
+		
+		
+        
+
+}
+}
+
